98c3abc6d5749040b251ad1523b7356e76c1ba44
Christian Sarnataro <christian.sarnataro@vimnmix.com>
2020-03-06 18:29:10 +0100

    Fixed indentation.

--- src/LatestWorks.elm
+++ src/LatestWorks.elm
@@ -1,13 +1,11 @@
 module LatestWorks exposing (Msg, update, view, init)
 
-import Models exposing(..)
+import Models exposing(Lang(..), Area(..), Work, Model, listOfWorksDecoder, initialWork)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Browser
 import Http exposing (..)
-import Json.Decode as D exposing (Decoder, field, string, int)
-
 
 main : Program () Model Msg
 main =
@@ -19,39 +17,6 @@ main =
     }
 
 
-initialWork: Work
-initialWork =   {
-    id = -1, 
-    language = Italian, 
-    area = Technical,
-    title = "",
-    description = "",
-    order = 0
-  }
-
-
-latestWorks: List Work
-latestWorks = [
-  {
-    id = 1, 
-    language = Italian, 
-    area = Technical,
-    title = "Test title",
-    description = "Long description",
-    order = 1
-  },
-  {
-    id = 2, 
-    language = Italian, 
-    area = Technical,
-    title = "Test title 2",
-    description = "Long description 2",
-    order = 2
-  }
-
-  ]
-
-
 subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.none
@@ -59,94 +24,69 @@ subscriptions model =
 
 init : () -> (Model, Cmd Msg)
 init _ =
-    ({ 
-      currentWork = initialWork,
-      latestWorks = []
-    },
-    getUsers
+    (
+      { currentWork = initialWork
+      , latestWorks = []
+      }
+      , getLatestWorks
     )
 
 
-getUsers: Cmd Msg
-getUsers =
+getLatestWorks: Cmd Msg
+getLatestWorks =
     Http.get
       { 
-        url = "https://reqres.in/api/users",
+        url = "http://www.graniteng.com/admin/latestWorks/api.php?login=admin&password=engineersecretgranit",
         expect = Http.expectJson GotText listOfWorksDecoder
       }
     
+processLoad : Model -> Int -> (Model, Cmd Msg)
+processLoad model id =
 
--- userDecoder : Decoder (List Work)
--- userDecoder =
---   field "data" (list (field "email" string))
-
-
-workDecoder : D.Decoder Work
-workDecoder =
-    D.map6
-        Work
-        (D.field "id" D.int)
-        (D.succeed Italian)
-        (D.succeed Technical)
-        (D.field "id" D.int)
-        (D.field "email" D.string)
-        (D.field "first_name" D.string)
-
-    -- id : Int,
-    -- language: Lang,
-    -- area: Area,
-    -- order: Int,
-    -- title: String,
-    -- description: String
+    case finder model.latestWorks id of
+      Nothing -> (model, Cmd.none)
+      Just w -> 
+        (
+          { model | currentWork = w }
+          , Cmd.none
+        )
 
 
-listOfWorksDecoder : D.Decoder (List Work)
-listOfWorksDecoder =
-    field "data" (D.list workDecoder)
+processJson : Model -> Result Error (List Work) -> (Model, Cmd Msg)
+processJson model result = 
+  case result of
+    Ok value -> 
+      (
+        { model | latestWorks = value }
+        , Cmd.none
+      )
+    Err err ->
+      ( 
+        {
+          model 
+          | latestWorks = [
+            { id = 1
+            , language = Italian
+            , area = Technical
+            , title = "Error"
+            , description = 
+              case err of 
+                BadBody str -> str
+                _ -> "Unknown error"
+            , order = 1
+        }]
+      } , Cmd.none)
 
-type Msg
-    = Load Int
-    | GotText (Result Http.Error (List Work))
-    | Create
-    | Delete
+
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Load id ->
-          case finder model.latestWorks id of
-            Nothing -> (model, Cmd.none)
-            Just w -> ({model | currentWork = w}, Cmd.none)
-
-        Create ->
-          (model, Cmd.none)
-
-        Delete -> 
-          (model, Cmd.none)
-        GotText result -> 
-          case result of
-            Ok value -> (
-              {
-                model 
-              | latestWorks = value
-              }, Cmd.none)
-            Err err -> ( {
-                model 
-              | latestWorks = [
-                {
-                  id = 1, 
-                  language = Italian, 
-                  area = Technical,
-                  title = case err of 
-                    BadBody str ->
-                      str
-                    _ -> "Error"
-                  ,
-                  description = "Long description",
-                  order = 1
-                }]
-              } , Cmd.none)
+        Load id -> processLoad model id
+        Create -> (model, Cmd.none)
+        Delete ->  (model, Cmd.none)
+        GotText result -> processJson model result
 
 view : Model -> Html Msg
 view model =
@@ -158,6 +98,17 @@ view model =
             (\work -> tr []
               [
                 td[][text (String.fromInt work.id)],
+                td[][text (
+                  case work.area of
+                    Technical -> "area tecnica"
+                    Training -> "formazione"
+                    Management -> "management"
+                )],
+                td[][text (
+                  case work.language of 
+                    English -> "English"
+                    Italian -> "Italiano"
+                )],
                 td[][text work.title],
                 td[][text work.description],
                 td[][
@@ -173,20 +124,54 @@ workForm: Work -> Html Msg
 workForm work = 
   Html.form[][
     span [] [text (String.fromInt work.id)],
-    label[for "title"][text "Title"],
+    hr [] [],
+    label[for "title"][text "Titolo"],
     input[value work.title, name "title", id "title"][],
-    input[value work.description][],
+    label[for "language"][text "Lingua"],
+    select[name "language", id "language"] (List.map toLangOption [
+      ((Italian, "Italiano"), work.language), 
+      ((English, "Inglese"), work.language)
+    ]),
+    label[for "area"][text "Area"],
+    select[name "area", id "area"] (List.map toAreaOption [
+      ((Technical, "Area tecnica"), work.area), 
+      ((Training, "Formazione"), work.area),
+      ((Management, "Management"), work.area)
+    ]),
+    label[for "description"][text "Descrizione"],
+    input[id "description", value work.description][],
     input[type_ "hidden", value (String.fromInt work.id)][]
   ]
 
--- toTableRow: Work -> Html Msg
--- toTableRow myListItem =
---   tr []
---      [
---      td[][text (String.fromInt myListItem.id)],
---      td[][text myListItem.title]
---      ]
+toLangOption: ((Lang, String), Lang) -> Html msg
+toLangOption ((value_, label), currentValue)  = 
+  option [value 
+  (
+    case value_ of 
+      Italian -> "it"
+      English -> "en"
+  ) , selected 
+    (  value_ == currentValue
+    )] [text label]
 
+toAreaOption: ((Area, String), Area) -> Html msg
+toAreaOption ((value_, label), currentValue)  = 
+  option [value 
+  (
+    case value_ of 
+      Training -> "training"
+      Management -> "management"
+      Technical -> "technical"
+  ) , selected 
+    (  value_ == currentValue
+    )] [text label]
+
+
+type Msg
+    = Load Int
+    | GotText (Result Http.Error (List Work))
+    | Create
+    | Delete
 
 finder : List Work -> Int -> Maybe Work
 finder lst id = 
--- src/Models.elm
+++ src/Models.elm
@@ -1,4 +1,6 @@
-module Models exposing (Model, Work, Area(..), Lang(..))
+module Models exposing (Model, Work, Area(..), Lang(..), listOfWorksDecoder, initialWork)
+
+import Json.Decode as D exposing (field, string, int)
 
 
 type alias Model = 
@@ -25,3 +27,56 @@ type alias Work =
     description: String
   }
 
+initialWork: Work
+initialWork =   {
+    id = -1, 
+    language = Italian, 
+    area = Technical,
+    title = "",
+    description = "",
+    order = 0
+  }
+
+
+
+workDecoder : D.Decoder Work
+workDecoder =
+    D.map6
+        Work
+        (D.field "id" D.int)
+        (D.field "language" languageDecoder)
+        (D.field "area" areaDecoder)
+        (D.field "row_order" D.int)
+        (D.field "title" D.string)
+        (D.field "description" D.string)
+
+languageDecoder: D.Decoder Lang
+languageDecoder = 
+    D.string
+        |> D.andThen (\str ->
+           case str of
+                "en" ->
+                    D.succeed English
+                _ ->
+                    D.succeed Italian
+        )
+
+areaDecoder: D.Decoder Area
+areaDecoder = 
+    D.string
+        |> D.andThen (\str ->
+           case str of
+                "technical" ->
+                    D.succeed Technical
+                "training" ->
+                    D.succeed Training
+                "management" ->
+                    D.succeed Management
+                other -> D.fail <| "Unknown area: " ++ other
+        )
+
+
+
+listOfWorksDecoder : D.Decoder (List Work)
+listOfWorksDecoder =
+    D.list workDecoder
